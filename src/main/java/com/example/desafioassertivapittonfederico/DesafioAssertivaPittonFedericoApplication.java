bc2c72791893e4f498bf79487a6221d829d0e1f4
FedericoPitton <fedejpitton@gmail.com>
2023-06-26 21:57:58 -0300

    Creacion del desafio para Assertiva

---
+++ src/main/java/com/example/desafioassertivapittonfederico/DesafioAssertivaPittonFedericoApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioassertivapittonfederico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAssertivaPittonFedericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAssertivaPittonFedericoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioassertivapittonfederico/DesafioAssertivaPittonFedericoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioassertivapittonfederico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAssertivaPittonFedericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
